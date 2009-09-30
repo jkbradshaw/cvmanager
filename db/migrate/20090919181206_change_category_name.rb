7389ec6ea41dff64e2d342bcc33023a44e221806
John Bradshaw <johnkbradshaw@yahoo.com>
2009-09-29 22:10:12 -0400

    updated readme

--- db/migrate/20090919181206_change_category_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCategoryName < ActiveRecord::Migration
-  def self.up
-    rename_column :cme_categories, :name, :category
-  end
-
-  def self.down
-    rename_column :cme_categories, :category, :name
-  end
-end
--- db/migrate/20090927175257_add_trainee_to_authors.rb
+++
@@ -1,9 +0,0 @@
-class AddTraineeToAuthors < ActiveRecord::Migration
-  def self.up
-    add_column :authors, :trainee, :boolean, :default=>false
-  end
-
-  def self.down
-    remove_column :authors, :trainee
-  end
-end
